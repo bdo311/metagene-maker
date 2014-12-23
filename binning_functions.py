30e17b95811d247af39a7be8030488a29fe95de8
Brian Do <bdo@stanford.edu>
2014-12-22 16:26:27 -0800

    all ends of blocks, reads, bins are non inclusive; updated blocks

--- binning_functions.py
+++ binning_functions.py
@@ -8,8 +8,9 @@ from datetime import datetime
 logger = logging.getLogger('')
 
 # gets the average score within this bin
+# end of bin is NOT inclusive, because end of read in bedgraph is also NOT inclusive
 def getAverageScore(readsForChrom, currStart, currEnd, binNum, rn):
-	totalLength = currEnd - currStart + 1
+	totalLength = currEnd - currStart
 	totalScore = 0
 	readNumber = rn 			# which read to start at
 
@@ -36,11 +37,11 @@ def getAverageScore(readsForChrom, currStart, currEnd, binNum, rn):
 
 		if currStart < readStart: currStart = readStart # ignore zeros that are in the read region
 		if readEnd >= currEnd: 	# end of gene is within the read
-			totalScore += score * (currEnd - currStart + 1)
+			totalScore += score * (currEnd - currStart)
 			#print 'totalScore', totalScore, currEnd - currStart + 1
 			break				# don't advance read number because the read extends past end of gene
 		else: # end of gene is past the read
-			totalScore += score * (readEnd - currStart + 1)
+			totalScore += score * (readEnd - currStart)
 			#print 'totalScore', totalScore, readEnd - currStart + 1
 			readNumber += 1 	# advance read number because this read won't be needed anymore
 
@@ -48,12 +49,7 @@ def getAverageScore(readsForChrom, currStart, currEnd, binNum, rn):
 
 	return float(totalScore)/totalLength, readNumber, binNum
 
-# gets the array of bins for each gene
-def getBins(start, end, numBins, readsForChrom, binLength):
-
-	currStart = start
-	binNum = start/binLength    
-
+def getInitialReadNumber(readsForChrom, binNum, currStart):
 	# better way to find the initial readnumber; saves a few seconds
 	readNumber = 0 #index of read, updated each time
 	readList = readsForChrom[binNum]
@@ -66,33 +62,29 @@ def getBins(start, end, numBins, readsForChrom, binLength):
 		#time.sleep(0.3)
 		if curr == left or curr == right: break
 		if currStart < readList[curr][0]: right = curr
-		else: left = curr					
-	
+		else: left = curr	
+
+	return curr
+
+
+# gets the array of bins for each gene
+# end of gene is NOT inclusive
+def getBins(start, end, numBins, readsForChrom, binLength):
+
 	# walking through each bin for the region
-	# tstart = datetime.now()
-	# for i in range(100):
 	scores = []
-	readNumber = curr-1		
+	readNumber = getInitialReadNumber(readsForChrom, binNum, currStart) - 1
+	currStart = start
+	binNum = start/binLength    	
 	spacingPerBin = int(math.ceil((end - start)/float(numBins)))
 
 	while currStart < end:
-		currEnd = currStart + spacingPerBin - 1 # end of my window
+		currEnd = currStart + spacingPerBin # end of my window
 		if currEnd > end: currEnd = end # last bin can't go past TES
 		
-		# xstart = datetime.now()
-		# for i in range(100):
 		score, readNumber, binNum = getAverageScore(readsForChrom, currStart, currEnd, binNum, readNumber) #updates read number also
-		# xend = datetime.now()
-		# delta = xend - xstart
-		# print delta.total_seconds()
-		
-		#print readNumber, currStart, currEnd, readList[readNumber]
-		#time.sleep(0.3)
 		scores.append(score)
-		currStart = currEnd + 1 # new start of my window
-	# tend = datetime.now()
-	# delta = tend - tstart
-	# print delta.total_seconds()
+		currStart = currEnd # new start of my window
 
 	return scores
 
@@ -137,52 +129,66 @@ def regionWorker(binFolder, regionType, chrom, chrToIndivRegions, stranded, fold
 	
 
 # gets the array of bins for each gene
+# start and end of each block are NOT inclusive
 def blockGetBins(blocks, numBins, readsForChrom, binLength):
-
-	currStart = start
-	binNum = start/binLength    
-
-	# better way to find the initial readnumber; saves a few seconds
-	readNumber = 0 #index of read, updated each time
-	readList = readsForChrom[binNum]
-	left = 0
-	right = len(readList) - 1	
-	curr = 0
-	while True:
-		curr = (left + right)/2
-		if curr == left or curr == right: break
-		if currStart < readList[curr][0]: right = curr
-		else: left = curr					
-	
-	# walking through each bin for the region
+		
+	# variables that will not change
 	scores = []
-	readNumber = curr-1		
 	totalLength = sum([bk[1]-bk[0] for bk in blocks])
 	end = blocks[-1][1]
 	spacingPerBin = int(math.ceil(totalLength/float(numBins)))
 
 	# blocks is [[block1s, block1e], [block2s, block2e], ...]
+	# variables that will change as the loop iterates
+	binNum = currStart/binLength    				
+	currStart = blocks[0][0]
+	readNumber = getInitialReadNumber(readsForChrom, binNum, currStart) - 1
+		
 	blockNum = 0
 	contBlock = False
-	spacingLeft = spacingPerBin
-	retainedScore = 0
-	while currStart < end:
-		blockStart, blockEnd = blocks[blockNum][0], blocks[blockNum][1]
-		currEnd = currStart + spacingPerBin - 1 # end of my window
-		if contBlock:
-			contBlock = False
-			currEnd = 
-		if currEnd > end: currEnd = end # last bin can't go past TES
-		if currEnd > blockEnd: 
-			contBlock = True
-			blockNum += 1
-			spacingLeft = currEnd - blockEnd
-			currEnd 
-		
-		score, readNumber, binNum = getAverageScore(readsForChrom, currStart, currEnd, binNum, readNumber) 
-		scores.append(score)
-		currStart = currEnd + 1 # new start of my window
+	oldPartialLength = 0
+	oldPartialScore = 0
 
+	while True:
+		blockStart, blockEnd = blocks[blockNum][0], blocks[blockNum][1] #info about current block
+		currEnd = currStart + spacingLeft
+			
+		if currEnd >= end: currEnd = end # last bin can't go past TES
+		if currEnd <= blockEnd:
+			print "case 1: ", currStart, currEnd
+			score,readNumber,binNum = getAverageScore(readsForChrom, currStart, currEnd, binNum, readNumber)
+			newPartialLength = currEnd - currStart
+			score = (score * newPartialLength + partialScore * oldPartialLength)/float(spacingPerBin) #weighted average
+			scores.append(score)
+			
+			# reset things
+			contBlock = False
+			spacingLeft = spacingPerBin
+			oldPartialLength = 0
+			oldPartialScore = 0
+			
+			# define start for next iteration of while loop
+			if currEnd < blockEnd: currStart = currEnd # new start of my window
+			else: 
+				if currEnd == end: break
+				blockNum += 1
+				currStart = blocks[blockNum][0]
+		else:
+			print "case 2: ", currStart, blockEnd, spacingLeft\
+			currEnd = blockEnd
+			score,readNumber,binNum = getAverageScore(readsForChrom, currStart, currEnd, binNum, readNumber)
+			newPartialLength = currEnd - currStart
+			score = (score * newPartialLength + partialScore * oldPartialLength)/float(newPartialLength + oldPartialLength) #weighted average
+			
+			# carry over to new block
+			contBlock = True
+			oldPartialScore = score
+			oldPartialLength = oldPartialLength + newPartialLength		# remaining bin length is reduced
+			blockNum += 1
+			
+			# define start for next iteration of while loop
+			currStart = blocks[blockNum][0] # start at the next bin
+		
 	return scores
 
 # for each chromosome, get bins corresponding to each region in the chromosome
