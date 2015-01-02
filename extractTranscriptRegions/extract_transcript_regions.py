dd7f538cf229a99a617fed1e61d11d02e05a276b
Brian Do <bdo@stanford.edu>
2015-01-01 16:20:43 -0800

    updated extractTranscriptRegions

--- extractTranscriptRegions/extract_transcript_regions.py
+++ extractTranscriptRegions/extract_transcript_regions.py
@@ -1,92 +1,101 @@
 #!/usr/bin/env python
 
-# Stephen N. Floor 
-# 7 October 2014 
-# floor@berkeley.edu 
+# Stephen N. Floor
+# 7 October 2014
+# floor@berkeley.edu
 
-# TODO: 
+# TODO:
 
 # convert UCSC gene names to refseq?
-# add a region type which is mrna that contains the whole spliced transcript, and preserve cdsStart and cdsEnd for these.  - this is the same as codingExons, just need to preserve start/stop 
+# add a region type which is mrna that contains the whole spliced transcript, and preserve cdsStart and cdsEnd for these.  - this is the same as codingExons, just need to preserve start/stop
+# remove pandas from GTF.py 
 
-import sys, os, csv
-from UCSCKnownGene import * 
+import sys, os, argparse, csv, glob
 csv.register_dialect("textdialect", delimiter='\t') #BTD added for later parsing
+import GTF
+from collections import defaultdict 
+from Transcript import *
+
+print " ----------------------------------"
+print "| Extract Regions from annotations |"
+print "|  snf        Fall 2014            |"
+print " ----------------------------------\n\n"
+
+
+# ------ ARGUMENT PARSING ----------
+
+parser = argparse.ArgumentParser(description="Create transcript regions (5' UTR/CDS/3'UTR etc) from knownGenes or a GTF") 
+
+parser.add_argument("-i", "--input", help="input filename", required=True)
+parser.add_argument("-o", "--output", help="output basename", required=True) 
+parser.add_argument("--ucsc", help="Read from a UCSC knownGenes formatted file (BED)", action="store_true")
+parser.add_argument("--gtf", help="Read from a GTF (only tested with Ensembl GTFs)", action="store_true")
+
+args = parser.parse_args() 
+
+if ( (not (args.ucsc or args.gtf)) or (args.ucsc and args.gtf)):
+    sys.exit("FATAL: must set one but not both of --ucsc and --gtf") 
+
+# output filenames:
+utr5FName = args.output + "_5utr.bed"
+utr5StartFName = args.output + "_5utr_start.bed"
+cdsFName = args.output + "_cds.bed"
+utr3FName = args.output + "_3utr.bed"
+exonFName = args.output + "_exons.bed"
+intronFName = args.output + "_introns.bed"
+codingExonFName = args.output + "_codingexons.bed"
+codingIntronFName = args.output + "_codingintrons.bed" # note that these are introns from coding genes, not necessarily introns that make it to mRNA
+noncodingExonFName = args.output + "_noncodingexons.bed"
+noncodingIntronFName = args.output + "_noncodingintrons.bed"
+
+#keep track of where we are
+
+# parameters that should be passed via the cmd line
+useBlocks = True
+
+
+# #terminate if output files exist
+
+# if os.path.exists(utr5FName) or os.path.exists(utr5StartFName) or os.path.exists(cdsFName) or os.path.exists(utr3FName) or os.path.exists(exonFName) or os.path.exists(intronFName) \
+        # or os.path.exists(codingExonFName) or os.path.exists(codingIntronFName) or os.path.exists(noncodingExonFName) or os.path.exists(noncodingIntronFName):
+    # sys.exit("ERROR: output basename %s files already exist" % args.output)
+
+# #process the file
 
 def main():
-	print " ---------------------------------"
-	print "| Extract Regions from knownGenes |"
-	print "|  snf   7 October 2014           |"
-	print " ---------------------------------\n\n"
-
-	if len(sys.argv) != 3:
-		sys.exit("ERROR: Please provide the knownGenes file and output basename as inputs")
-
-
-	# output filenames: 
-	utr5FName = sys.argv[2] + "_5utr.bed"
-	utr5StartFName = sys.argv[2] + "_5utr_start.bed"
-	cdsFName = sys.argv[2] + "_cds.bed"
-	utr3FName = sys.argv[2] + "_3utr.bed"
-	exonFName = sys.argv[2] + "_exons.bed"
-	intronFName = sys.argv[2] + "_introns.bed"
-	codingExonFName = sys.argv[2] + "_codingexons.bed"
-	codingIntronFName = sys.argv[2] + "_codingintrons.bed" # note that these are introns from coding genes, not necessarily introns that make it to mRNA 
-	noncodingExonFName = sys.argv[2] + "_noncodingexons.bed" 
-	noncodingIntronFName = sys.argv[2] + "_noncodingintrons.bed" 
-
-	#keep track of where we are 
-	genesRead = 0
-
-	# parameters that should be passed via the cmd line
-	useBlocks = True 
-
-
-	# #terminate if output files exist
-
-	# if os.path.exists(utr5FName) or os.path.exists(utr5StartFName) or os.path.exists(cdsFName) or os.path.exists(utr3FName) or os.path.exists(exonFName) or os.path.exists(intronFName) \
-			# or os.path.exists(codingExonFName) or os.path.exists(codingIntronFName) or os.path.exists(noncodingExonFName) or os.path.exists(noncodingIntronFName):
-		# sys.exit("ERROR: output basename %s files already exist" % sys.argv[2]) 
-
-	# #process the file
-
-	with open(sys.argv[1]) as knownGenesFile, open(utr5FName, "w") as utr5File, open(utr5StartFName, "w") as utr5StartFile, open(cdsFName, "w") as cdsFile, \
+	with open(utr5FName, "w") as utr5File, open(utr5StartFName, "w") as utr5StartFile, open(cdsFName, "w") as cdsFile, \
 			open(utr3FName, "w") as utr3File, open(exonFName, "w") as exonFile, open (intronFName, "w") as intronFile, \
 			open(codingExonFName, "w") as codingExonFile, open(codingIntronFName, "w") as codingIntronFile, \
 			open(noncodingExonFName, "w") as noncodingExonFile, open(noncodingIntronFName, "w") as noncodingIntronFile:
 
-		for line in knownGenesFile:
-			# all of the knowngenes parsing and metadata construction is done inside UCSCKnownGene.py, especially the createGene method 
-			gene = createGene(line)
-			genesRead += 1
-
-			if (useBlocks): # output all region primitives on the same line by specifying nBlocks and lists inside the BED output 
-				if(gene.coding): 
-					#blockBedFormat is one line by definition 
+		def writeOutput(gene):
+			if (useBlocks): # output all region primitives on the same line by specifying nBlocks and lists inside the BED output
+				if(gene.coding):
+					#blockBedFormat is one line by definition
 					if (gene.utr5Len > 0): utr5File.write(gene.blockBedFormat(region="5utr") + "\n")
 					if (gene.utr5startLen > 0): utr5StartFile.write(gene.blockBedFormat(region="5utr_start") + "\n")
 					if (gene.cdsLen > 0): cdsFile.write(gene.blockBedFormat(region="cds") + "\n")
 					if (gene.utr3Len > 0): utr3File.write(gene.blockBedFormat(region="3utr") + "\n")
-					
-					if (gene.exonsLen > 0): 
+				
+					if (gene.exonsLen > 0):
 						exonFile.write(gene.blockBedFormat(region="exons") + "\n")
 						codingExonFile.write(gene.blockBedFormat(region="exons") + "\n")
 					
 					if (gene.intronsLen > 0):
 						intronFile.write(gene.blockBedFormat(region="introns") + "\n")
 						codingIntronFile.write(gene.blockBedFormat(region="introns") + "\n")
-				
-				else: # noncoding transcripts just have exons and introns 
+						
+				else: # noncoding transcripts just have exons and introns
 					if (gene.exonsLen > 0):
 						exonFile.write(gene.blockBedFormat(region="exons") + "\n")
 						noncodingExonFile.write(gene.blockBedFormat(region="exons") + "\n")
-					
+
 					if (gene.intronsLen > 0):
 						intronFile.write(gene.blockBedFormat(region="introns") + "\n")
 						noncodingIntronFile.write(gene.blockBedFormat(region="introns") + "\n")
 
-			else: # output one line per region primitive instead of combining regions via blocks 
-				if(gene.coding): 
+			else: # output one line per region primitive instead of combining regions via blocks
+				if(gene.coding):
 					for entry in gene.bedFormat(region="5utr"):
 						utr5File.write(entry + "\n")
 					for entry in gene.bedFormat(region="5utr_start"):
@@ -99,45 +108,95 @@ def main():
 					for entry in gene.bedFormat(region="exons"):
 						exonFile.write(entry + "\n")
 						codingExonFile.write(entry + "\n")
-				
+
 					for entry in gene.bedFormat(region="introns"):
 						intronFile.write(entry + "\n")
 						codingIntronFile.write(entry + "\n")
-				
-				else: # noncoding transcripts just have exons and introns 
+
+				else: # noncoding transcripts just have exons and introns
 					for entry in gene.bedFormat(region="exons"):
 						exonFile.write(entry + "\n")
 						noncodingExonFile.write(entry + "\n")
-				
+
 					for entry in gene.bedFormat(region="introns"):
 						intronFile.write(entry + "\n")
 						noncodingIntronFile.write(entry + "\n")
 
-			if (not genesRead % 2500):
-				print "Processed %d entries..." %  genesRead
+
+		if (args.ucsc): 
+			with open(args.input, "r") as genesFile: 
+				genesRead = 0
+
+				for line in genesFile:
+					# all of the knowngenes parsing and metadata construction is done inside UCSCKnownGene.py, especially the createGene method
+
+					gene = createUCSCTranscript(line) 
+					genesRead += 1
+
+					writeOutput(gene)
+
+					if (not genesRead % 2500):
+						print "Processed %d entries..." %  genesRead
+
+					
+		elif (args.gtf): 
+				
+				# first parse the entire file into a dictionary of lists
+
+			txDict = defaultdict(list) 
+
+			print "Building GTF dictionary..." 
+
+			# the issue here is that lines for various transcripts may be interleaved, so can either create lots of SNFGene objects, or a giant dict. opted for giant dict. 
+			for line in GTF.lines(args.input): 
+
+				txDict[line["transcript_id"]].append(line)
+				genesRead += 1
+
+				if (not genesRead % 100000):
+					print "Processed %d lines..." %  genesRead
+
+			print "Dictionary built." 
+
+			# now create a SNFGene object for each transcript and output it 
+			genesRead = 0
+			for key in txDict: 
+
+				#print key
+
+				tx = createGTFTranscript(txDict[key])
+
+				#print tx 
+				writeOutput(tx)
+				genesRead += 1
+				
+				if (not genesRead % 2500):
+					print "Processed %d entries..." %  genesRead
+
 
 	print "Processed %d entries." %  genesRead
 
 	# BTD Edit: making unique regions and linking to gene name
 	# --------------------------------------------------------
-	# utr5FName = sys.argv[2] + "_5utr.bed"
-	# utr5StartFName = sys.argv[2] + "_5utr_start.bed"
-	# cdsFName = sys.argv[2] + "_cds.bed"
-	# utr3FName = sys.argv[2] + "_3utr.bed"
-	# exonFName = sys.argv[2] + "_exons.bed"
-	# intronFName = sys.argv[2] + "_introns.bed"
-	# codingExonFName = sys.argv[2] + "_codingexons.bed"
-	# codingIntronFName = sys.argv[2] + "_codingintrons.bed" # note that these are introns from coding genes, not necessarily introns that make it to mRNA 
-	# noncodingExonFName = sys.argv[2] + "_noncodingexons.bed" 
-	# noncodingIntronFName = sys.argv[2] + "_noncodingintrons.bed" 
+	# utr5FName = args.output  + "_5utr.bed"
+	# utr5StartFName = args.output  + "_5utr_start.bed"
+	# cdsFName = args.output  + "_cds.bed"
+	# utr3FName = args.output  + "_3utr.bed"
+	# exonFName = args.output  + "_exons.bed"
+	# intronFName = args.output  + "_introns.bed"
+	# codingExonFName = args.output  + "_codingexons.bed"
+	# codingIntronFName = args.output  + "_codingintrons.bed" # note that these are introns from coding genes, not necessarily introns that make it to mRNA 
+	# noncodingExonFName = args.output  + "_noncodingexons.bed" 
+	# noncodingIntronFName = args.output  + "_noncodingintrons.bed" 
 
 	# 1. Get gene ID (NM_123, ENSG123) --> gene name (Abcd1)
 	print "Getting gene ID"
 	idToName = {}
-	with open(sys.argv[1], 'r') as knownGeneFile:
-		reader = csv.reader(knownGeneFile, 'textdialect')
-		for row in reader:
-			idToName[row[0]] = row[-1]
+	if args.ucsc:
+		with open(args.input, 'r') as knownGeneFile:
+			reader = csv.reader(knownGeneFile, 'textdialect')
+			for row in reader:
+				idToName[row[0]] = row[-1]
 			
 	# 2. Get unique 5'UTR, 5'Start UTR, and 3'UTR
 	print "Getting unique UTRs"
@@ -150,18 +209,20 @@ def main():
 				if tuple(row[6:]) in already: continue #repeat
 				geneIDInfo = row[3]
 				id = geneIDInfo.split('__')[0]
-				geneName = idToName[id]
-				row[3] = geneIDInfo + '__' + geneName
+				try: geneName = idToName[id]
+				except: geneName = id
+				if geneName != id: row[3] = id + '__' + geneName
+				else: row[3] = id
 				already.add(tuple(row[6:]))
 				writer.writerow(row)
 				
-	uniq5UTR = sys.argv[2] + "_uniq_5utr.bed"
+	uniq5UTR = args.output  + "_uniq_5utr.bed"
 	getUniqUTR(uniq5UTR, utr5FName)
 
-	uniq3UTR = sys.argv[2] + '_uniq_3utr.bed'
+	uniq3UTR = args.output  + '_uniq_3utr.bed'
 	getUniqUTR(uniq3UTR, utr3FName)
 
-	uniq5SUTR = sys.argv[2] + '_uniq_5utr_start.bed'
+	uniq5SUTR = args.output  + '_uniq_5utr_start.bed'
 	getUniqUTR(uniq5SUTR, utr5StartFName)
 		
 	# 3. Get unique exons + num. Do it 3x for all, coding, and noncoding
@@ -175,8 +236,10 @@ def main():
 				# gene ID info
 				geneIDInfo = row[3]
 				id = geneIDInfo.split('__')[0]
-				geneName = idToName[id]
-				geneIDInfo = geneIDInfo + '__' + geneName
+				try: geneName = idToName[id]
+				except: geneName = id
+				if geneName != id: geneIDInfo = id + '__' + geneName
+				else: geneIDInfo = id
 				
 				# chrom, start, stop, strand
 				chrom = row[0]
@@ -206,18 +269,18 @@ def main():
 					
 					# unique
 					if tuple(outputRow) in already: continue
-					already.add(tuple(outputRow))			
-					outputRow.extend([geneIDInfo, exonNum, strand])
+					already.add(tuple(outputRow))            
+					outputRow.extend([geneIDInfo + '__exon' + str(exonNum), 0, strand])
 					writer.writerow(outputRow)
 				
-	uniqExons = sys.argv[2] + '_uniq_exons.bed'
+	uniqExons = args.output  + '_uniq_exons.bed'
 	getUniqExons(uniqExons, exonFName)
 
-	uniqExons = sys.argv[2] + '_uniq_codingexons.bed'
+	uniqExons = args.output  + '_uniq_codingexons.bed'
 	getUniqExons(uniqExons, codingExonFName)
 
-	uniqExons = sys.argv[2] + '_uniq_noncodingexons.bed'
-	getUniqExons(uniqExons, noncodingExonFName)			
+	uniqExons = args.output  + '_uniq_noncodingexons.bed'
+	getUniqExons(uniqExons, noncodingExonFName)            
 
 	# 4. Get unique introns + num. unique 5'SS, 3'SS. 
 	# 5'SS is first base of intron, 3'SS is last base of intron
@@ -238,8 +301,10 @@ def main():
 				# gene ID info
 				geneIDInfo = row[3]
 				id = geneIDInfo.split('__')[0]
-				geneName = idToName[id]
-				geneIDInfo = geneIDInfo + '__' + geneName
+				try: geneName = idToName[id]
+				except: geneName = id
+				if geneName != id: geneIDInfo = id + '__' + geneName
+				else: geneIDInfo = id
 				
 				# chrom, start, stop, strand
 				chrom = row[0]
@@ -270,7 +335,7 @@ def main():
 					# unique introns
 					if tuple(outputRow) in alreadyIntron: continue
 					alreadyIntron.add(tuple(outputRow))
-					outputRow.extend([geneIDInfo, intronNum, strand])
+					outputRow.extend([geneIDInfo+ '__intron' + str(intronNum), strand])
 					intronWriter.writerow(outputRow)
 					
 					# unique splice sites
@@ -282,26 +347,26 @@ def main():
 						fiveSS = [chrom, intron[1] - 1, intron[1]]
 					if tuple(fiveSS) not in already5:
 						already5.add(tuple(fiveSS))
-						fiveSS.extend([geneIDInfo, intronNum, strand])
+						fiveSS.extend([geneIDInfo + '__5ss' + str(intronNum), 0, strand])
 						fiveWriter.writerow(fiveSS)
 					if tuple(threeSS) not in already3:
 						already3.add(tuple(threeSS))
-						threeSS.extend([geneIDInfo, intronNum, strand])
+						threeSS.extend([geneIDInfo+ '__3ss' + str(intronNum), 0, strand])
 						threeWriter.writerow(threeSS)
 
-	uniqIntrons = sys.argv[2] + '_uniq_introns.bed'
-	uniq5 = sys.argv[2] + '_uniq_5ss.bed'
-	uniq3 = sys.argv[2] + '_uniq_3ss.bed'
+	uniqIntrons = args.output  + '_uniq_introns.bed'
+	uniq5 = args.output  + '_uniq_5ss.bed'
+	uniq3 = args.output  + '_uniq_3ss.bed'
 	getUniqIntronsAndSS(uniqIntrons, uniq5, uniq3, intronFName)
 
-	uniqIntrons = sys.argv[2] + '_uniq_codingintrons.bed'
-	uniq5 = sys.argv[2] + '_uniq_coding5ss.bed'
-	uniq3 = sys.argv[2] + '_uniq_coding3ss.bed'
+	uniqIntrons = args.output  + '_uniq_codingintrons.bed'
+	uniq5 = args.output  + '_uniq_coding5ss.bed'
+	uniq3 = args.output  + '_uniq_coding3ss.bed'
 	getUniqIntronsAndSS(uniqIntrons, uniq5, uniq3, codingIntronFName)
 
-	uniqIntrons = sys.argv[2] + '_uniq_noncodingintrons.bed'
-	uniq5 = sys.argv[2] + '_uniq_noncoding5ss.bed'
-	uniq3 = sys.argv[2] + '_uniq_noncoding3ss.bed'
+	uniqIntrons = args.output  + '_uniq_noncodingintrons.bed'
+	uniq5 = args.output  + '_uniq_noncoding5ss.bed'
+	uniq3 = args.output  + '_uniq_noncoding3ss.bed'
 	getUniqIntronsAndSS(uniqIntrons, uniq5, uniq3, noncodingIntronFName)
 
 	# 5. unique TSS/TES
@@ -316,8 +381,10 @@ def main():
 			for row in reader:
 				geneIDInfo = row[3]
 				id = geneIDInfo.split('__')[0]
-				geneName = idToName[id]
-				geneIDInfo = geneIDInfo + '__' + geneName
+				try: geneName = idToName[id]
+				except: geneName = id
+				if geneName != id: geneIDInfo = id + '__' + geneName
+				else: geneIDInfo = id
 				
 				# chrom, start, stop, strand
 				chrom = row[0]
@@ -337,12 +404,17 @@ def main():
 				if tuple(endRow) not in alreadyTSS:
 					alreadyTES.add(tuple(endRow))
 					endRow.extend([geneIDInfo, 0, strand])
-					tesWriter.writerow(endRow)			
+					tesWriter.writerow(endRow)            
 				
-	uniqTSS = sys.argv[2] + '_uniq_tss.bed'
-	uniqTES = sys.argv[2] + '_uniq_tes.bed'
+	uniqTSS = args.output  + '_uniq_tss.bed'
+	uniqTES = args.output  + '_uniq_tes.bed'
 	getUniqTSSAndTES(uniqTSS, uniqTES, cdsFName)
-	
-if __name__ == '__main__':
+
+
+	# sort everything
+	print "Sorting BED files"
+	for fn in glob.glob("*.bed"):
+		os.system("sort -k1,1 -k2,2n %s -o %s"%(fn, fn))
+		
+if __name__=='__main__':
 	main()
-	
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 import sys, site, os
 
 setup(name='metagenemaker',
-      version='0.3.1',
+      version='0.4',
       description='Obtain average profiles of NGS datasets over your favorite regions',
       
 	  install_requires = ['numpy>=1.7', 'pandas>=0.14'],
 	  
-	  packages=['metagene_maker', 'knownGenes'],
-	  package_dir={'metagene_maker': 'metagene_maker', 'knownGenes': 'knownGenes'},
+	  packages=['metagene_maker', 'extractTranscriptRegions'],
+	  package_dir={'metagene_maker': 'metagene_maker', 'extractTranscriptRegions': 'extractTranscriptRegions'},
 	  
-	  entry_points = {'console_scripts': ['metagene_maker = metagene_maker.metagene_maker:main', 'knownGenes = knownGenes.knowngenes_to_transcript_regions:main']},
+	  entry_points = {'console_scripts': ['metagene_maker = metagene_maker.metagene_maker:main', 'extractTranscriptRegions = extractTranscriptRegions.extract_transcript_regions:main']},
 	  
 	  author='Brian Do',
       author_email='bdo@stanford.edu',
